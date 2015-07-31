2918555ca0afe756f67399c431fce267b5b30218
sheigel <eigel.si@gmail.com>
2015-07-31 23:14:36 +0300

    changed deploy.js to not return crap

--- deploy.js
+++ deploy.js
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === 'production') {
 
   // We basically just create a child process that will run
   // the production bundle command
-  var child_process = require('child_process');
-  return child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+  var childProcess = require('child_process');
+   childProcess.exec('webpack -p --config webpack.production.config.js', function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
